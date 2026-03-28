34d142188aa52eb84c0e35bdea79d6af11dd34bc
Matias Araoz <araozmatias2010@gmail.com>
2026-03-28 11:31:14 -0300

    Changed and added some scripts

--- wallpaperchanger.sh
+++ wallpaperchanger.sh
@@ -3,7 +3,14 @@ WALLPAPERS_DIR="$HOME/Wallpapers/"
 while true; do
 	wallpapers=($WALLPAPERS_DIR/*)
 	SELECTED_WALL=$(printf "%s\n" "${wallpapers[@]}" | shuf -n 1)
-	hyprpanel sw "$SELECTED_WALL"
+	swww img $SELECTED_WALL
+	matugen image "$SELECTED_WALL" \
+        -m dark \
+        -t scheme-content \
+        --source-color-index 0 \
+        -j hex | jq -r '.colors | to_entries | .[] | "@define-color \(.key) \(.value.dark.color);"' > ~/.config/waybar/colors.css
 
+	sleep 0.5
+	systemctl --user restart waybar.service
 	sleep 600
 done
